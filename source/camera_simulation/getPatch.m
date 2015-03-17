5d3382f378fbb54aa455dd29c3ba3b49f2a28e81
Marcel Geppert <mgeppert@student.ethz.ch>
2015-03-17 18:14:56 +0100

    changed camera simultion to use interpolation between source image pixels (much slower now)

--- source/camera_simulation/getPatch.m
+++ source/camera_simulation/getPatch.m
@@ -6,17 +6,29 @@ function patch = getPatch(img, K, alpha, beta, gamma)
 % and z-axis in image space (z axis points away from camera)
 
 patch = uint8(zeros(128));
-invK = inv(K);
 
-origin = round(size(img)/2);
+origin = size(img)/2;
 
 for u = 1:128
     for v = 1:128
-        invKP = invK*[u v 1]';
+%         invKP = invK*[u v 1]';
+
+%         compute ray angle through pixel
+        invKP = K \ [u v 1]';
         thetaAlpha = atan(cos(-gamma)*invKP(2) + sin(-gamma)*invKP(1));
         thetaBeta = -atan(cos(-gamma)*invKP(1) - sin(-gamma)*invKP(2));
         targetP = [beta + thetaBeta, alpha + thetaAlpha]; 
         
-        patch(v, u) = img(round(targetP(2)*size(img, 2)/(2*pi)) + origin(1), round(targetP(1)*size(img, 2)/(2*pi)) + origin(2));
+%         compute coordinates in input image
+        targetCoords = [targetP(2)*size(img, 2)/(2*pi) + origin(1), targetP(1)*size(img, 2)/(2*pi) + origin(2)];
+        roundedCoords = round(targetCoords);
+        
+        values = img(roundedCoords(1)-1:roundedCoords(1)+1, roundedCoords(2)-1:roundedCoords(2)+1);
+        coordOffset = targetCoords - roundedCoords + 2;
+        
+%         interpolate to obtain exact values
+        patch(v, u) = interp2(values, coordOffset(1), coordOffset(2));
+        
+%         patch(v, u) = img(round(targetP(2)*size(img, 2)/(2*pi)) + origin(1), round(targetP(1)*size(img, 2)/(2*pi)) + origin(2));
     end
 end
