c5426c5b060a75e51bd7197bf6049f6a6fba3c3c
iliis <samuelbryner@gmx.ch>
2015-05-11 16:54:06 +0200

    Draw checkerboard pattern. Flicker with VSYNC.

--- source/camera_calibration/main.cpp
+++ source/camera_calibration/main.cpp
@@ -6,21 +6,41 @@
 
 using namespace std;
 
+void draw_checkerboard_pattern(SDL_Window* window, SDL_Renderer* r, int numrows, int numcols)
+{
+    int W = 0, H = 0;
+    SDL_GetWindowSize(window, &W, &H);
+
+    SDL_Rect rect;
+    rect.w = W/numcols; rect.h = H/numrows;
+
+    for (int row = 0; row < numrows; ++row) {
+        for (int col = 0; col < numcols; ++col) {
+            if ((row % 2) == (col % 2)) {
+                rect.x = col * rect.w;
+                rect.y = row * rect.h;
+                SDL_RenderFillRect(r, &rect);
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     SDL_Window* window = NULL;
     window = SDL_CreateWindow
     (
-        "DVS Calibration Target", SDL_WINDOWPOS_UNDEFINED,
+        "DVS Calibration Target",
+        SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640,
         480,
-        SDL_WINDOW_SHOWN
+        SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN_DESKTOP
     );
 
     // Setup renderer
     SDL_Renderer* renderer = NULL;
-    renderer =  SDL_CreateRenderer( window, 0, SDL_RENDERER_ACCELERATED);
+    renderer =  SDL_CreateRenderer( window, 0, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
 
     SDL_Event e;
     bool quit = false;
@@ -35,27 +55,19 @@ int main(int argc, char* argv[])
             }
         }
 
-        // clear background
+        // show pattern
         SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
         SDL_RenderClear( renderer );
 
-        SDL_Rect rect;
-        rect.x = 100; rect.y = 100;
-        rect.w = 100; rect.h = 100;
-
         SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-        SDL_RenderFillRect(renderer, &rect);
-
-        // Render the rect to the screen
+        draw_checkerboard_pattern(window, renderer, 6, 10);
         SDL_RenderPresent(renderer);
 
-        SDL_Delay(1000);
 
-        // clear background
+        // show black window
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255 );
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
-        SDL_Delay(1000);
     }
 
     SDL_DestroyWindow(window);
