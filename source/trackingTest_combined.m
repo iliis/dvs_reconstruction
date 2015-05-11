17c4c879f4cf1a33a04c45423816e061683a9572
mgprt <mgeppert@eth.edu>
2015-05-11 15:53:16 +0200

    fixed order of arguments when calling poisson_solver_function

--- source/trackingTest_combined.m
+++ source/trackingTest_combined.m
@@ -30,7 +30,7 @@ outputImageSize = [500, 1000];
 boundary_image = 0.5*ones(outputImageSize);
 origin = outputImageSize ./ 2;
 gradients = zeros([2, outputImageSize]);
-covariances = 100*repmat(eye(2), [1, 1, outputImageSize]);
+covariances = 10*repmat(eye(2), [1, 1, outputImageSize]);
 lastSigs = zeros(128);
 
 % lastPos = round(reshape(cameraToWorldCoordinatesBatch(getInvKPsforPatch(cameraIntrinsicParameterMatrix()), [0 0 0], outputImageSize)', [2 128 128]));
@@ -55,7 +55,7 @@ secToLastPos = lastPos;
 %      [gradients, covariances, lastSigs, lastPos] = updateMosaic(events(i,1), events(i,2), events(i,3), events(i,4), [0 0 0], gradients, covariances, lastSigs, fakeLastPos);
 % end
 % pgrads = permute(gradients, [2 3 1]);
-% map = poisson_solver_function(pgrads(:,:,1), pgrads(:,:,2), boundary_image);
+% map = poisson_solver_function(pgrads(:,:,2), pgrads(:,:,1), boundary_image);
 % if ~exist('intermediate_map_figure', 'var') || ~ishandle(intermediate_map_figure)
 %     intermediate_map_figure = figure();
 % else
@@ -65,12 +65,12 @@ secToLastPos = lastPos;
 % % colormap('gray');
 % drawnow;
 
-[map, nextInd] = integrateInitialEvents(events, 10000, outputImageSize);
+[map, nextInd] = integrateInitialEvents(events, 1000, outputImageSize);
 
 % pgrads = permute(gradients, [2 3 1]);
 % size(pgrads)
 % size(boundary_image)
-% map = poisson_solver_function(pgrads(:,:,1), pgrads(:,:,2), boundary_image);
+% map = poisson_solver_function(pgrads(:,:,2), pgrads(:,:,1), boundary_image);
 % gradients = zeros([2, outputImageSize]);
 
 
@@ -96,7 +96,7 @@ theta_est = zeros(size(events, 1), 3);
 % end
 
 % pgrads = permute(gradients, [2 3 1]);
-% map = poisson_solver_function(pgrads(:,:,1), pgrads(:,:,2), boundary_image);
+% map = poisson_solver_function(pgrads(:,:,2), pgrads(:,:,1), boundary_image);
 % last_timestamp = TS(10000);
 % map = boundary_image;
 
@@ -105,8 +105,9 @@ lastPosUpdated = false;
 % initialPatch = cameraToWorldCoordinatesBatch(getInvKPsforPatch(cameraIntrinsicParameterMatrix()), [0 0 0], outputImageSize);
 originInitial = round(cameraToWorldCoordinates(64, 64, cameraIntrinsicParameterMatrix(), [0 0 0], outputImageSize));
 movementDetectedTimestamp = 1000000000;
+useGeneratedMap = false;
 
-for i = nextInd:size(events,1)
+for i = 1:size(events,1)
     
     deltaT_global = events(i,4) - last_timestamp;
     last_timestamp = events(i,4);
@@ -119,21 +120,23 @@ for i = nextInd:size(events,1)
 %     if ~lastPosUpdated && (sum(sum(initialPatch ~= cameraToWorldCoordinatesBatch(getInvKPsforPatch(cameraIntrinsicParameterMatrix()), theta_est(i,:), outputImageSize))) > (outputImageSize(1) * outputImageSize(2) / 2))
     if ~lastPosUpdated && (sum(abs(originInitial - round(cameraToWorldCoordinates(64, 64, cameraIntrinsicParameterMatrix(), theta_est(i,:), outputImageSize)))) > 5)
         lastPosUpdated = true;
-        movementDetectedTimestamp = TS(i);
+%         movementDetectedTimestamp = TS(i);
         newLastPos = round(reshape(cameraToWorldCoordinatesBatch(getInvKPsforPatch(cameraIntrinsicParameterMatrix()), [0 0 0], outputImageSize)', [2 128 128]));
-        lastPossize = size(lastPos);
-        newLastPosZize = size(newLastPos);
-        covariances = 100*repmat(eye(2), [1, 1, outputImageSize]);
+%         lastPossize = size(lastPos);
+%         newLastPosZize = size(newLastPos);
+        covariances = 10*repmat(eye(2), [1, 1, outputImageSize]);
         lastPos(lastPos == repmat(origin', [1 128 128])) = newLastPos(lastPos == repmat(origin', [1 128 128]));
     end
     
     [gradients, covariances, lastSigs, lastPos, secToLastSigs, secToLastPos] = updateMosaic(events(i,1), events(i,2), events(i,3), events(i,4), theta_est(i,:), gradients, covariances, lastSigs, lastPos, secToLastSigs, secToLastPos);
     
-    if (movementDetectedTimestamp + 100000 < i) && mod(i,100) == 0
+    if useGeneratedMap && mod(i,100) == 0
         pgrads = permute(gradients, [2 3 1]);
-        map = poisson_solver_function(pgrads(:,:,1), pgrads(:,:,2), boundary_image);
+        map = poisson_solver_function(pgrads(:,:,2), pgrads(:,:,1), boundary_image);
         disp(['map extreme values: [' num2str(min(min(map))) ', ' num2str(max(max(map))) ']']);
 %         disp(['image extreme values: [' num2str(min(min(img))) ', ' num2str(max(max(img))) ']']);
+    elseif ~useGeneratedMap
+        useGeneratedMap = norm(theta_est(i,1:2)) > 0.3;
     end
     
     %     if deltaT_global > 0; [map, ~] = reconstructMosaic(events_raw(1:i), TS(1:i), theta_est(1:i, :)); end;
@@ -154,7 +157,7 @@ for i = nextInd:size(events,1)
     
     if mod(i, 500) == 0
         if ~exist('intermediate_map_figure', 'var') || ~ishandle(intermediate_map_figure)
-            intermediate_map_figure = figure();
+            intermediate_map_figure = figure('Name', 'Currently used map');
         else
             figure(intermediate_map_figure);
         end
@@ -162,12 +165,12 @@ for i = nextInd:size(events,1)
         drawnow;
         
         if ~exist('new_map_figure', 'var') || ~ishandle(new_map_figure)
-            new_map_figure = figure();
+            new_map_figure = figure('Name', 'Build map');
         else
             figure(new_map_figure);
         end
         pgrads = permute(gradients, [2 3 1]);
-        newMap = poisson_solver_function(pgrads(:,:,1), pgrads(:,:,2), boundary_image);
+        newMap = poisson_solver_function(pgrads(:,:,2), pgrads(:,:,1), boundary_image);
         imagesc(newMap);
         colorbar;
         colormap('gray');
@@ -176,12 +179,12 @@ for i = nextInd:size(events,1)
     
     if mod(i, 1000) == 0
         
-        if ~exist('tracking_test2_figure', 'var') || ~ishandle(tracking_test2_figure)
-            tracking_test2_figure = figure();
-        else
-            figure(tracking_test2_figure);
-        end
-        plotParticles(particles, theta_gt(i,:)); drawnow; %waitforbuttonpress;
+%         if ~exist('tracking_test2_figure', 'var') || ~ishandle(tracking_test2_figure)
+%             tracking_test2_figure = figure('Name', 'Particles');
+%         else
+%             figure(tracking_test2_figure);
+%         end
+%         plotParticles(particles, theta_gt(i,:)); drawnow; %waitforbuttonpress;
         
         if ~exist('scaled_map_figure', 'var') || ~ishandle(scaled_map_figure)
             scaled_map_figure = figure('Name', 'scaled map');
@@ -197,7 +200,7 @@ for i = nextInd:size(events,1)
         else
             figure(map_gradient_figure);
         end
-        [X Y] = meshgrid(1:size(gradients,3), 1:size(gradients,2));
+        [X, Y] = meshgrid(1:size(gradients,3), 1:size(gradients,2));
         quiver(X, Y, permute(gradients(1,:,:), [2 3 1]), permute(gradients(2,:,:), [2 3 1]));
         
         drawnow;
@@ -237,7 +240,7 @@ legend('total error', 'error relative to overall movement', 'overall movement');
 
 
 pgrads = permute(gradients, [2 3 1]);
-map = poisson_solver_function(pgrads(:,:,1), pgrads(:,:,2), boundary_image);
+map = poisson_solver_function(pgrads(:,:,2), pgrads(:,:,1), boundary_image);
 disp(['map extreme values: [' num2str(min(min(map))) ', ' num2str(max(max(map))) ']']);
 disp(['image extreme values: [' num2str(min(min(img))) ', ' num2str(max(max(img))) ']']);
 
