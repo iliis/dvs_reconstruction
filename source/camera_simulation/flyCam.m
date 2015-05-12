785a6dce9c53578652fdd80001b602f4226c4df1
mgprt <mgeppert@eth.edu>
2015-05-12 19:38:13 +0200

    fixed flyCam to help debugging

--- source/camera_simulation/flyCam.m
+++ source/camera_simulation/flyCam.m
@@ -2,9 +2,10 @@ function flyCam(imagepath)
 
 % Testfunction to simulate camera rotation
 
-testimg = rgb2gray(imread(imagepath));
-K = [87.5 0 64.5; 0 87.5 64.5; 0 0 1];
+testimg = im2double(rgb2gray(imread(imagepath)));
+K = cameraIntrinsicParameterMatrix();
 for beta = -pi/4*3:0.02:pi/4*3
-    imshow(getPatch(testimg, K, 0, beta, 0));
-    pause(0.001);
+    invKPs = getInvKPsforPatch(K);
+    imshow(getPatch(testimg, invKPs, [0 beta 0]));
+    drawnow;
 end
