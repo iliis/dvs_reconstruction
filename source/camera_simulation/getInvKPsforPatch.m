e31d2041178e848f646776d4fd20cd638f662289
mgprt <mgeppert@eth.edu>
2015-05-13 17:36:22 +0200

    fixed some leftover size bugs

--- source/camera_simulation/getInvKPsforPatch.m
+++ source/camera_simulation/getInvKPsforPatch.m
@@ -1,7 +1,7 @@
 function invKPs = getInvKPsforPatch( K )
 
-invKPs = zeros([128 128 2]);
-for u = 1:164
+invKPs = zeros([64 64 2]);
+for u = 1:64
     for v = 1:64      
         invKP = K \ [u v 1]';  
         invKPs(v, u, :) = invKP(1:2);
