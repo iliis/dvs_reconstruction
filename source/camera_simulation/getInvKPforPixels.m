83c8d640b856c01b70bdd0b7afeba790e9182c32
mgprt <mgeppert@eth.edu>
2015-05-26 15:10:49 +0200

    fixed index-offset in getInvKPsforPatch (would fail for camera sizes other than 64^2) do we need to do this for getInvKPforPixels too? (do we need this function at all in the final system?)

--- source/camera_simulation/getInvKPforPixels.m
+++ source/camera_simulation/getInvKPforPixels.m
@@ -2,6 +2,8 @@ function [ invKPs ] = getInvKPforPixels( K, UV )
 % Input: Kamera Intrinsic matrix, Nx2 pixel coordinates [u, v]
 % output Nx1x2 matrix of inverted projected pixels
 
+% TODO: this also needs pixel offsets, doesn't it?
+
 invKPs = zeros([size(UV,1) 1 2]);
 for i = 1:size(UV,1)
     invKP = K \ [UV(i,1) UV(i,1) 1]';
--- source/camera_simulation/getInvKPsforPatch.m
+++ source/camera_simulation/getInvKPsforPatch.m
@@ -1,9 +1,11 @@
 function invKPs = getInvKPsforPatch( K )
+% TODO: use global K directly?
 
 invKPs = zeros([simulationPatchSize() simulationPatchSize() 2]);
+indexOffset = (128 - simulationPatchSize())/2;
 for u = 1:simulationPatchSize()
     for v = 1:simulationPatchSize()
-        invKP = K \ [u+simulationPatchSize()/2 v+simulationPatchSize()/2 1]';  
+        invKP = K \ [u+indexOffset v+indexOffset 1]';  
         invKPs(v, u, :) = invKP(1:2);
     end
 end
