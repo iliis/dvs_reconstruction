d54ff1cceed6111c64bb057ad35adc4802d3e619
mgprt <mgeppert@eth.edu>
2015-05-26 17:58:07 +0200

    fixed indexOffset in cameraToWorldCoordinates

--- source/camera_simulation/cameraToWorldCoordinates.m
+++ source/camera_simulation/cameraToWorldCoordinates.m
@@ -2,8 +2,9 @@ function [ world_coordinates ] = cameraToWorldCoordinates( u, v, K, theta, img_s
 
 assert(all([u > 0, u <= simulationPatchSize(), v > 0, v <= simulationPatchSize()]));
 
-u = u + simulationPatchSize()/2;
-v = v + simulationPatchSize()/2;
+indexOffset = (128 - simulationPatchSize())/2;
+u = u + indexOffset;
+v = v + indexOffset;
 
 invKPs = reshape(K \ double([u v 1]'), 1, 1, 3);
 %world_coordinates = cameraToWorldCoordinatesBatch_mex(invKPs(:,:,1:2), theta, img_size);
